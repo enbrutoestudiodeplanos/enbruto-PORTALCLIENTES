c9434bd41106335bf96f040b82fe2cf9ae863d4f
EN BRUTO <enbrutoestudiodeplanos@gmail.com>
2026-03-27 17:40:45 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwuK-Dk_si95n05XNxe10eLiyBtFKnJ6u-k48iyR2iH0OWQClsGpW3mwGQ7w-MCTOCMvg/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx87QvGiNpQW1NbBfUSGAQPHBEEWtExODZChyWfcx0ucefj30lSjoxiN5odeNUNhEITyA/exec';
 
 let orderSubmitting = false;
 let allOrders = [];
